ba90d97dc942ea08881f8dd7ccdb1bf3e4a24772
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-03-30 11:48:48 -0700

    Updates

---
+++ assets/index-yPIEQPQK.js
@@ -0,0 +1,9 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=c(e);fetch(e.href,r)}})();document.querySelector("#app").innerHTML=`
+  <div>
+    <h1>Pro Stock AI</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/xQKfP26Q5Oc?si=9AGxSCn-41GCokuY"
+      title="YouTube video player" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+`;
