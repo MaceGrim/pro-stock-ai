f6ae619a70ff0f35ea3fd5424b3e17c7df54df45
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-03-31 00:43:08 -0700

    Updates

--- assets/page2-D8kojRgS.js
+++
@@ -1,9 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css              */document.querySelector("#app").innerHTML=`
-  <div>
-    <h1>Pro Stock AI</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/xQKfP26Q5Oc?si=9AGxSCn-41GCokuY"
-      title="YouTube video player" frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
-`;
